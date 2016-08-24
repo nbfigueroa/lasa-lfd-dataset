89f2c3dbd2e57a8d6045f6fee652acdffa0d962e
nad <nbfigueroa@gmail.com>
2016-08-24 22:29:41 +0200

    Preprocess script for segmentation

--- Zucchini_Peeling/continuous/PreProcess_Bimanual_Data.m
+++ Zucchini_Peeling/continuous/PreProcess_Bimanual_Data.m
@@ -17,21 +17,23 @@ for ii=1:N
     plotBimanualTrajectories(proc_data{ii}); 
 
 end
-
-
+%% Move to tGau-BP-HMM Folder
+tgau_dir = '/home/nbfigueroa/dev/tGau-BP-HMM/experiments/Bimanual_Peeling';
+cd(tgau_dir)
 
 %% Preprocess for segmentation (Do BP-HMM on joint arm data + object Features)
 
 ChannelNames = {};
 
 Preproc.nObj   = N;
-Preproc.obsDim = 26;
+Preproc.obsDim = 32;
 Preproc.channelNames = ChannelNames;
 
 % Create data structure
 Bimanual_Arm_Data  = SeqData();
 
 force_sc = 15;
+color_sc = 100;
 
 for ii = 1:N
     
@@ -49,7 +51,6 @@ for ii = 1:N
      D_a = D_a';
      D_a = bsxfun( @minus, D_a, mean(D_a,2) );
      D_a = D_a';       
-  
     
     % %%%% Extract Data for Passive Arm %%%%%
     X_p = proc_data{ii}.passive.X;
@@ -60,18 +61,31 @@ for ii = 1:N
     D_p(4:7,:)    = X_p(4:7,:);
     D_p(8:10,:)   = X_p(8:10,:)/(force_sc*1.5);
     D_p(11:end,:) = X_p(11:end,:)/2;
-
+    
     % Enforce zero-mean, and apply block-averaging
      D_p = D_p';
      D_p = bsxfun( @minus, D_p, mean(D_p,2) );
      D_p = D_p';       
     
+    % %%%% Extract Data for Object %%%%%
+    X_o = proc_data{ii}.object.feats;
+    
+    % Scale Variables 
+    D_o = X_o;
+    D_o(1:3,:)    = X_o(1:3,:)/color_sc;    
+    D_o(4:end,:)  = X_o(4:end,:)/(color_sc/2);
+    
+    % Enforce zero-mean, and apply block-averaging
+    D_o = D_o';
+    D_o = bsxfun( @minus, D_o, mean(D_o,2) );
+    D_o = D_o';                 
+     
     % Add the sequence to our data structure
-    D = [D_a(:,1:length(D_p));D_p];
+    D = [D_a(:,1:length(D_p));D_p; D_o(:,1:length(D_p))];
     Bimanual_Arm_Data = Bimanual_Arm_Data.addSeq( D, proc_data{ii}.name);
         
 end
-fprintf( '... read in all %d sequences\n', Active_Arm_Data.N );
+fprintf( '... read in all %d sequences\n', Bimanual_Arm_Data.N );
 
 %% Preprocess for segmentation (Do BP-HMM on each arm independently)
 
